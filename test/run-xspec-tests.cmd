51338bf46226e81275261f04c2f6f38a9de30378
AirQuick <AirQuick@users.noreply.github.com>
2017-03-16 02:06:35 +0900

    Do not print search result
    just because run-xspec-tests.sh does not

--- test/run-xspec-tests.cmd
+++ test/run-xspec-tests.cmd
@@ -44,7 +44,7 @@ for %%I in (*.xspec) do (
     rem
     rem Inspect result
     rem
-    findstr /r /c:".*failed: [1-9]" "%RESULT_FILE%" || findstr /r /c:"\*\** Error [a-z][a-z]*ing the test suite" "%RESULT_FILE%"
+    ( findstr /r /c:".*failed: [1-9]" "%RESULT_FILE%" || findstr /r /c:"\*\** Error [a-z][a-z]*ing the test suite" "%RESULT_FILE%" ) > NUL
     if not errorlevel 1 (
         echo FAILED: %%~I
         if /i "%APPVEYOR%"=="True" appveyor UpdateTest "%%~I" -Framework custom -Filename "%~nx0" -Outcome Failed -Duration 0
