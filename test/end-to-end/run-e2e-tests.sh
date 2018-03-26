f74567b1983c7005507a6588a39745c7269553d1
Vincent Lizzi <vincent.lizzi@taylorandfrancis.com>
2018-03-26 01:24:49 -0400

    fix for previous commit

--- test/end-to-end/run-e2e-tests.sh
+++ test/end-to-end/run-e2e-tests.sh
@@ -11,18 +11,19 @@ export TEST_DIR=${CASES_DIR}/xspec
 # Run test cases
 for CASE_FILEPATH in ${CASES_DIR}/*.xspec
 do
+    CASE_FILENAME=${CASE_FILEPATH##*/}
+    CASE_BASENAME=${CASE_FILENAME%.xspec}
+
     # Generate the report HTML
-    if test "${CASE_FILEPATH:0:10}" = "schematron"; then
+    if test "${CASE_FILENAME:0:10}" = "schematron"; then
         ../../bin/xspec.sh -s ${CASE_FILEPATH} > /dev/null 2>&1
-    elif test "${CASE_FILEPATH:0:6}" = "xquery"; then
+    elif test "${CASE_FILENAME:0:6}" = "xquery"; then
         ../../bin/xspec.sh -q ${CASE_FILEPATH} > /dev/null 2>&1
     else
         ../../bin/xspec.sh ${CASE_FILEPATH} > /dev/null 2>&1
     fi
 
     # Compare with the expected HTML
-    CASE_FILENAME=${CASE_FILEPATH##*/}
-    CASE_BASENAME=${CASE_FILENAME%.xspec}
     if java -classpath ${SAXON_CP} net.sf.saxon.Transform -s:${TEST_DIR}/${CASE_BASENAME}-result.html -xsl:processor/compare.xsl | grep '^OK: Compared '
         then
             # OK, nothing to do
