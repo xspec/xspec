c7c65691c4bd9c66f999a12b4fde3ff44b31a4fe
AirQuick <AirQuick@users.noreply.github.com>
2018-06-25 15:16:02 +0900

    Schema: Exclude x:* from any-content (#175)
    * Install Jing
    * Test with schema
    * Exclude x:* from any-content
    * Do not validate test/schematron/*-out.xspec (they are invalid deliberately as discussed in #176)

--- test/xspec-bat.cmd
+++ test/xspec-bat.cmd
@@ -607,6 +607,31 @@ setlocal
     call :teardown
 endlocal
 
+setlocal
+    call :setup "Schema detects no error in tutorial"
+
+    if defined JING_CP (
+        call :run java -jar "%JING_CP%" -c ..\src\schemas\xspec.rnc ..\tutorial\*.xspec ..\tutorial\schematron\*.xspec
+        call :verify_retval 0
+    ) else (
+        call :skip "Schema validation for tutorial skipped"
+    )
+
+    call :teardown
+endlocal
+
+setlocal
+    call :setup "Schema detects no error in known good tests"
+
+    if defined JING_CP (
+        call :run java -jar "%JING_CP%" -c ..\src\schemas\xspec.rnc catalog\*.xspec schematron\*-import.xspec schematron\*-in.xspec
+        call :verify_retval 0
+    ) else (
+        call :skip "Schema validation for known good tests skipped"
+    )
+
+    call :teardown
+endlocal
 
 echo === END TEST CASES ==================================================
 
--- test/xspec.bats
+++ test/xspec.bats
@@ -484,3 +484,25 @@ teardown() {
 	[ "${lines[7]}" = "passed: 1 / pending: 0 / failed: 0 / total: 1" ]
 	rm -rf $SAXON_HOME
 }
+
+
+@test "Schema detects no error in tutorial" {
+    if [ -n "${JING_CP}" ]; then
+        run java -jar ${JING_CP} -c ../src/schemas/xspec.rnc ../tutorial/*.xspec ../tutorial/schematron/*.xspec
+    	echo "$output"
+        [ "$status" -eq 0 ]
+    else
+        skip "Schema validation for tutorial skipped";
+    fi
+}
+
+
+@test "Schema detects no error in known good tests" {
+    if [ -n "${JING_CP}" ]; then
+        run java -jar ${JING_CP} -c ../src/schemas/xspec.rnc catalog/*.xspec schematron/*-import.xspec schematron/*-in.xspec
+    	echo "$output"
+        [ "$status" -eq 0 ]
+    else
+        skip "Schema validation for known good tests skipped";
+    fi
+}
