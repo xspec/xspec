d341b7ff121689075f410031e3a36343393b6c59
AirQuick <AirQuick@users.noreply.github.com>
2018-08-06 09:31:33 +0900

    Fail deliberately

--- bin/xspec.sh
+++ bin/xspec.sh
@@ -399,7 +399,7 @@ elif test -n "$JUNIT"; then
 		|| die "Error formatting the JUnit report"
 	echo "Report available at $JUNIT_RESULT"
 else
-    echo "Report available at $HTML"
+    echo "Report TEST available at $HTML"
     #$OPEN "$HTML"
 fi
 
