95a28b65fa8eb3c4f8f216f3562e24a7154c0450
AirQuick <AirQuick@users.noreply.github.com>
2018-08-17 17:42:32 +0900

    Fail deliberately

--- bin/xspec.bat
+++ bin/xspec.bat
@@ -567,7 +567,7 @@ if defined COVERAGE (
     rem %OPEN% "%COVERAGE_HTML%"
 ) else if defined JUNIT (
     call :xslt -o:"%JUNIT_RESULT%" ^
-        -s:"%RESULT%" ^
+        -s:"TEST_%RESULT%" ^
         -xsl:"%XSPEC_HOME%\src\reporter\junit-report.xsl" ^
         || ( call :die "Error formatting the JUnit report" & goto :win_main_error_exit )
     call :win_echo "Report available at %JUNIT_RESULT%"
