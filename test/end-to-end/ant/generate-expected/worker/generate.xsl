2136f43c3ecaefd63f18c3562d776aabef897d64
AirQuick <AirQuick@users.noreply.github.com>
2018-09-04 17:39:37 +0900

    Use Ant for end-to-end tests

---
+++ test/end-to-end/ant/generate-expected/worker/generate.xsl
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet exclude-result-prefixes="#all" version="2.0"
+	xmlns:x="http://www.jenitennison.com/xslt/xspec" xmlns:xs="http://www.w3.org/2001/XMLSchema"
+	xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+	<!--
+		This stylesheet transforms a template of Ant build file into a working build file.
+		The basic transformation is done by the imported stylesheet.
+		In addition to the imported basic transformation, this stylesheet creates some Ant
+		nodes to normalize the XSpec report files.
+	-->
+
+	<!-- Import: Some may be overridden -->
+	<xsl:import href="../../base/worker/generate.xsl" />
+
+	<!--
+		Overrides an imported named template
+		
+		Inserts <normalize-xspec-report> into the default <post-task>.
+		Context node is in each .xspec file's /x:description/@*.
+	-->
+	<xsl:template as="element(normalize-xspec-report)+" name="on-post-task">
+		<xsl:param as="element(reports)" name="reports" required="yes" />
+
+		<!-- Normalize the actual report files -->
+		<xsl:for-each select="$reports/element()">
+			<normalize-xspec-report actual-report-url="{@actual}"
+				normalized-report-url="{@expected}"
+				normalizer-xsl-url="{resolve-uri('normalize.xsl', @processor-dir)}" />
+		</xsl:for-each>
+	</xsl:template>
+</xsl:stylesheet>
--- test/end-to-end/generate-expected.sh
+++ test/end-to-end/generate-expected.sh
@@ -1,39 +1,4 @@
 #!/bin/bash
-
-# This script must be run from the script's directory
-
-# Reset public environment variables
-export SAXON_CP="${SAXON_JAR}"
-unset SAXON_HOME
-
-# .xspec files directory
-CASES_DIR=cases
-
-# XSpec output directory
-export TEST_DIR=${CASES_DIR}/expected
-
-# Process .xspec files
-for CASE_FILEPATH in ${CASES_DIR}/*.xspec
-do
-    echo
-    echo "----------"
-    echo "Processing ${CASE_FILEPATH}..."
-
-    CASE_FILENAME=${CASE_FILEPATH##*/}
-    CASE_BASENAME=${CASE_FILENAME%.xspec}
-
-    # Generate the report HTML
-    if test "${CASE_FILENAME:0:10}" = "schematron"; then
-        ../../bin/xspec.sh -j -s ${CASE_FILEPATH}
-    elif test "${CASE_FILENAME:0:6}" = "xquery"; then
-        ../../bin/xspec.sh -j -q ${CASE_FILEPATH}
-    else
-        ../../bin/xspec.sh -j ${CASE_FILEPATH}
-    fi
-
-    # Normalize the report HTML
-    java -jar "${SAXON_JAR}" -o:${TEST_DIR}/${CASE_BASENAME}-result-norm.html -s:${TEST_DIR}/${CASE_BASENAME}-result.html -xsl:processor/html/normalize.xsl
-    
-    # Normalize the JUnit report
-    java -jar "${SAXON_JAR}" -o:${TEST_DIR}/${CASE_BASENAME}-junit-norm.xml -s:${TEST_DIR}/${CASE_BASENAME}-junit.xml -xsl:processor/junit/normalize.xsl
-done
+myname="${BASH_SOURCE:-$0}"
+mydir=$(cd -P -- $(dirname -- "${myname}"); pwd)
+ant -buildfile "${mydir}/ant/generate-expected/build.xml" -lib "${SAXON_JAR}" "$@"
