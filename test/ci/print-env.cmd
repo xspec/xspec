84bbc6c761d4228d5ee808f781cfda4d71e8de18
AirQuick <AirQuick@users.noreply.github.com>
2019-09-25 05:46:52 +0900

    ci: print environment (#647)

--- test/ci/print-env.cmd
+++ test/ci/print-env.cmd
@@ -1,11 +1,41 @@
-echo Print Java version
+@echo off
+
+setlocal
+
+echo:
+echo === Print Java version
 java -version
 
-echo Print Ant version
+echo:
+echo === Print Ant version
 call ant -version
 
-echo Print Saxon version
+echo:
+echo === Print Saxon version
 java -cp "%SAXON_JAR%" net.sf.saxon.Version
 
-echo Print environment variables
+echo:
+echo === Check XML Calabash
+java -jar "%XMLCALABASH_JAR%" 2> NUL
+
+echo:
+echo === Print XML Resolver version
+java -cp "%XML_RESOLVER_JAR%" org.apache.xml.resolver.Version
+
+echo:
+echo === Check Jing
+java -jar "%JING_JAR%"
+
+echo:
+echo === Check BaseX
+java -cp "%BASEX_JAR%" org.basex.BaseX -h
+
+echo:
+echo === Check BaseX server start and stop
+call "%BASEX_JAR%\..\bin\basexhttp.bat" -S
+call "%BASEX_JAR%\..\bin\basexhttpstop.bat"
+
+echo:
+echo === Print environment variables
 set
+
