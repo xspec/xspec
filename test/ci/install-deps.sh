4a52b8914a24abb362336fbabeca1ff6663f4777
AirQuick <AirQuick@users.noreply.github.com>
2019-09-25 10:03:42 +0900

    ci: set Saxon and XML Resolver path in test/ci/install-deps.* (#649)

--- test/ci/install-deps.sh
+++ test/ci/install-deps.sh
@@ -1,39 +1,46 @@
 #! /bin/bash
 
+# root dir
+if [ -z ${XSPEC_DEPS} ]; then
+    export XSPEC_DEPS=/tmp/xspec
+fi
+
 # install Saxon
+export SAXON_JAR=${XSPEC_DEPS}/saxon/saxon9he.jar
 curl -fsSL --create-dirs --retry 5 -o ${SAXON_JAR} http://central.maven.org/maven2/net/sf/saxon/Saxon-HE/${SAXON_VERSION}/Saxon-HE-${SAXON_VERSION}.jar
 
 # install XML Calabash
 if [ -z ${XMLCALABASH_VERSION} ]; then
     echo "XMLCalabash will not be installed as it uses a higher version of Saxon";
 else
-    curl -fsSL --create-dirs --retry 5 -o /tmp/xspec/xmlcalabash/xmlcalabash.zip https://github.com/ndw/xmlcalabash1/releases/download/${XMLCALABASH_VERSION}/xmlcalabash-${XMLCALABASH_VERSION}.zip;
-    unzip /tmp/xspec/xmlcalabash/xmlcalabash.zip -d /tmp/xspec/xmlcalabash;
-    export XMLCALABASH_JAR=/tmp/xspec/xmlcalabash/xmlcalabash-${XMLCALABASH_VERSION}/xmlcalabash-${XMLCALABASH_VERSION}.jar;
+    curl -fsSL --create-dirs --retry 5 -o ${XSPEC_DEPS}/xmlcalabash/xmlcalabash.zip https://github.com/ndw/xmlcalabash1/releases/download/${XMLCALABASH_VERSION}/xmlcalabash-${XMLCALABASH_VERSION}.zip;
+    unzip ${XSPEC_DEPS}/xmlcalabash/xmlcalabash.zip -d ${XSPEC_DEPS}/xmlcalabash;
+    export XMLCALABASH_JAR=${XSPEC_DEPS}/xmlcalabash/xmlcalabash-${XMLCALABASH_VERSION}/xmlcalabash-${XMLCALABASH_VERSION}.jar;
 fi
 
 # install BaseX
 if [[ -z ${XMLCALABASH_VERSION} && -z ${BASEX_VERSION} ]]; then
     echo "BaseX will not be installed as it requires to run XMLCalabash with a higher version of Saxon";
 else
-    curl -fsSL --create-dirs --retry 5 -o /tmp/xspec/basex/basex.zip http://files.basex.org/releases/${BASEX_VERSION}/BaseX${BASEX_VERSION//./}.zip;
-    unzip /tmp/xspec/basex/basex.zip -d /tmp/xspec/basex;
-    export BASEX_JAR=/tmp/xspec/basex/basex/BaseX.jar;
+    curl -fsSL --create-dirs --retry 5 -o ${XSPEC_DEPS}/basex/basex.zip http://files.basex.org/releases/${BASEX_VERSION}/BaseX${BASEX_VERSION//./}.zip;
+    unzip ${XSPEC_DEPS}/basex/basex.zip -d ${XSPEC_DEPS}/basex;
+    export BASEX_JAR=${XSPEC_DEPS}/basex/basex/BaseX.jar;
 fi
 
 # install Ant
-curl -fsSL --create-dirs --retry 5 -o /tmp/xspec/ant/ant.tar.gz http://archive.apache.org/dist/ant/binaries/apache-ant-${ANT_VERSION}-bin.tar.gz
-tar xf /tmp/xspec/ant/ant.tar.gz -C /tmp/xspec/ant;
-export ANT_HOME=/tmp/xspec/ant/apache-ant-${ANT_VERSION}
+curl -fsSL --create-dirs --retry 5 -o ${XSPEC_DEPS}/ant/ant.tar.gz http://archive.apache.org/dist/ant/binaries/apache-ant-${ANT_VERSION}-bin.tar.gz
+tar xf ${XSPEC_DEPS}/ant/ant.tar.gz -C ${XSPEC_DEPS}/ant;
+export ANT_HOME=${XSPEC_DEPS}/ant/apache-ant-${ANT_VERSION}
 export PATH=${ANT_HOME}/bin:${PATH}
 
 # install XML Resolver
+export XML_RESOLVER_JAR=${XSPEC_DEPS}/xml-resolver/resolver.jar
 curl -fsSL --create-dirs --retry 5 -o ${XML_RESOLVER_JAR} http://central.maven.org/maven2/xml-resolver/xml-resolver/1.2/xml-resolver-1.2.jar
 
 #install Jing
 if [ -z ${JING_VERSION} ]; then
     echo "Jing will not be installed";
 else
-    export JING_JAR=/tmp/xspec/jing/jing.jar;
+    export JING_JAR=${XSPEC_DEPS}/jing/jing.jar;
     curl -fsSL --create-dirs --retry 5 -o ${JING_JAR} http://central.maven.org/maven2/org/relaxng/jing/${JING_VERSION}/jing-${JING_VERSION}.jar;
 fi
