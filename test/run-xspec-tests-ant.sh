e28e084c29215bd43edcc198e2d26499b816f19e
AirQuick <AirQuick@users.noreply.github.com>
2019-10-02 23:37:17 +0900

    ci: externalize *nix tests further

--- test/run-xspec-tests-ant.sh
+++ test/run-xspec-tests-ant.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-ant -buildfile ant/build.xml -lib "${SAXON_JAR}" "$@"
+myname="${BASH_SOURCE:-$0}"
+mydir=$(cd -P -- $(dirname -- "${myname}"); pwd)
+ant -buildfile "${mydir}/ant/build.xml" -lib "${SAXON_JAR}" "$@"
