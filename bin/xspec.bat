0acc6475c1d261bcd0a361a4a9773dc7c29706bd
AirQuick <AirQuick@users.noreply.github.com>
2019-11-08 09:00:33 +0900

    Remove EXPath file module from coverage-report.xsl (#680)

--- bin/xspec.bat
+++ bin/xspec.bat
@@ -73,6 +73,7 @@ rem ##
     java ^
         -Dxspec.coverage.ignore="%TEST_DIR%" ^
         -Dxspec.coverage.xml="%COVERAGE_XML%" ^
+        -Dxspec.xspecfile="%XSPEC%" ^
         -cp "%CP%" net.sf.saxon.Transform %CATALOG% %*
     goto :EOF
 
@@ -80,6 +81,7 @@ rem ##
     java ^
         -Dxspec.coverage.ignore="%TEST_DIR%" ^
         -Dxspec.coverage.xml="%COVERAGE_XML%" ^
+        -Dxspec.xspecfile="%XSPEC%" ^
         -cp "%CP%" net.sf.saxon.Query %CATALOG% %*
     goto :EOF
 
@@ -563,7 +565,6 @@ if defined COVERAGE (
         -o:"%COVERAGE_HTML%" ^
         -s:"%COVERAGE_XML%" ^
         -xsl:"%XSPEC_HOME%\src\reporter\coverage-report.xsl" ^
-        tests="%XSPEC%" ^
         inline-css=true ^
         || ( call :die "Error formatting the coverage report" & goto :win_main_error_exit )
     call :win_echo "Report available at %COVERAGE_HTML%"
