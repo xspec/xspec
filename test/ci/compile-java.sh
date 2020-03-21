8b9030605d35c24d928a031af77092307c20cb94
AirQuick <AirQuick@users.noreply.github.com>
2020-03-22 02:34:56 +0900

    ci: delegate Azure Pipelines condition to cmd

--- test/ci/compile-java.sh
+++ test/ci/compile-java.sh
@@ -7,5 +7,5 @@ if javac -version 2>&1 | grep -F ' 1.8.'; then
 	mydir=$(cd -P -- $(dirname -- "${myname}"); pwd)
 	ant -buildfile "${mydir}/build_java.xml" "$@"
 else
-	echo "Skip compiling java"
+	echo "Skip compiling Java"
 fi
