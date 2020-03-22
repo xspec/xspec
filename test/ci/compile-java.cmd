a119a8578d4612b9463608314f4a69588c930842
AirQuick <AirQuick@users.noreply.github.com>
2020-03-22 09:54:39 +0900

    ci: skip compiling Java with Saxon 10

--- test/ci/compile-java.cmd
+++ test/ci/compile-java.cmd
@@ -2,8 +2,14 @@ echo Compile Java
 
 javac -version 2>&1 | %SYSTEMROOT%\system32\find " 1.8."
 if errorlevel 1 (
-    echo Skip compiling Java
+    echo Skip compiling with incompatible JDK
     verify > NUL
-) else (
-    call ant -buildfile "%~dp0build_java.xml" %*
+    goto :EOF
 )
+
+if not "%SAXON_VERSION:~0,2%"=="9." (
+    echo Skip compiling with incompatible Saxon
+    goto :EOF
+)
+
+call ant -buildfile "%~dp0build_java.xml" %*
