c59a1eee8debebadcfe1645440e1c0aeae6a691b
AirQuick <AirQuick@users.noreply.github.com>
2020-03-28 16:09:08 +0900

    ci: harden CI scripts

--- test/ci/switch-java.cmd
+++ test/ci/switch-java.cmd
@@ -1,9 +1,6 @@
 echo on
 
-if not defined AGENT_OSARCHITECTURE (
-  verify other 2> NUL
-  goto :EOF
-)
+if not defined AGENT_OSARCHITECTURE exit /b 1
 
 rem "call" is to resolve the target environment variable such as JAVA_HOME_11_X64 dynamically
 call set "JAVA_HOME=%%JAVA_HOME_%~1_%AGENT_OSARCHITECTURE%%%"
