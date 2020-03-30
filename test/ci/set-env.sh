e4d620c07d450e9985cef2fdb9d801fec4083e58
AirQuick <AirQuick@users.noreply.github.com>
2020-03-31 01:16:27 +0900

    ci: install deps using Ant (#883)

--- test/ci/set-env.sh
+++ test/ci/set-env.sh
@@ -4,7 +4,7 @@
 # Select the mainstream by default
 #
 if [ -z "${XSPEC_TEST_ENV}" ]; then
-    XSPEC_TEST_ENV=saxon-9-9
+    export XSPEC_TEST_ENV=saxon-9-9
 fi
 echo "Setting up ${XSPEC_TEST_ENV}"
 
@@ -20,27 +20,50 @@ for f in \
 do
     # * "Set environment variables from file of key/value pairs": https://stackoverflow.com/a/49674707
     # * "Process substitution to 'source' do not work on Mac OS": https://stackoverflow.com/a/56060300
-    declares=$(egrep -v '^#|^$' "${f}" | sed -e 's/.*/declare -x "&"/g')
-    eval "${declares}"
+    declares=$(egrep -v '^#|^$|^[^=]+=$' "${f}" | sed -e 's/.*/declare -x "&"/g')
+    if [ -n "${declares}" ]; then
+        echo "${declares}"
+        eval "${declares}"
+    fi
+
+    unsets=$(egrep -v '^#' "${f}" | egrep '^[^=]+=$' | sed -e 's/=$//' -e 's/.*/unset &/')
+    if [ -n "${unsets}" ]; then
+        echo "${unsets}"
+        eval "${unsets}"
+    fi
 done
 
 #
 # Root dir
 #
-if [ -z "${XSPEC_DEPS}" ]; then
-    export XSPEC_DEPS=/tmp/xspec
+if [ -z "${XSPEC_TEST_DEPS}" ]; then
+    export XSPEC_TEST_DEPS=/tmp/xspec-test-deps
 fi
 
 #
 # Ant
 #
-export ANT_HOME="${XSPEC_DEPS}/ant/apache-ant-${ANT_VERSION}"
-export PATH="${ANT_HOME}/bin:${PATH}"
+
+# Depends on the archive file structure
+export ANT_HOME="${XSPEC_TEST_DEPS}/apache-ant-${ANT_VERSION}"
+
+# Path component
+ant_bin="${ANT_HOME}/bin"
+
+# Remove from PATH
+# https://unix.stackexchange.com/a/108933
+PATH=":$PATH:"
+PATH="${PATH//:${ant_bin}:/:}"
+PATH="${PATH#:}"
+PATH="${PATH%:}"
+
+# Prepend to PATH
+export PATH="${ant_bin}:${PATH}"
 
 #
 # Saxon
 #
-SAXON_JAR="${XSPEC_DEPS}/saxon"
+SAXON_JAR="${XSPEC_TEST_DEPS}/saxon-${SAXON_VERSION}"
 
 # Keep the original (not Maven) file name convention so that we can test SAXON_HOME properly
 if [ "${SAXON_VERSION:0:2}" = "9." ]; then
@@ -52,24 +75,26 @@ fi
 #
 # XML Resolver
 #
-export XML_RESOLVER_JAR="${XSPEC_DEPS}/xml-resolver/resolver.jar"
+export XML_RESOLVER_JAR="${XSPEC_TEST_DEPS}/xml-resolver-${XML_RESOLVER_VERSION}/resolver.jar"
 
 #
 # XML Calabash
 #
 if [ -n "${XMLCALABASH_VERSION}" ]; then
-    # Depends on the zip file structure
-    export XMLCALABASH_JAR="${XSPEC_DEPS}/xmlcalabash/xmlcalabash-${XMLCALABASH_VERSION}/xmlcalabash-${XMLCALABASH_VERSION}.jar"
+    # Depends on the archive file structure
+    export XMLCALABASH_JAR="${XSPEC_TEST_DEPS}/xmlcalabash-${XMLCALABASH_VERSION}/xmlcalabash-${XMLCALABASH_VERSION}.jar"
 else
     echo "XML Calabash will not be installed"
+    unset XMLCALABASH_JAR
 fi
 
 #
 # BaseX
 #
 if [ -n "${BASEX_VERSION}" ]; then
-    # Depends on the zip file structure
-    export BASEX_JAR="${XSPEC_DEPS}/basex/basex/BaseX.jar"
+    # Depends on the archive file structure
+    export BASEX_JAR="${XSPEC_TEST_DEPS}/basex-${BASEX_VERSION}/basex/BaseX.jar"
 else
     echo "BaseX will not be installed"
+    unset BASEX_JAR
 fi
