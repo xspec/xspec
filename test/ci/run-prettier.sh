c0b52971d16e11ceed107dd6fa7adf7bafce88fd
AirQuick <AirQuick@users.noreply.github.com>
2020-04-25 10:05:30 +0900

    ci: name npm scripts with colon

--- test/ci/run-prettier.sh
+++ test/ci/run-prettier.sh
@@ -6,10 +6,10 @@ echo "Run prettier"
 npm ci || exit
 
 # Check by prettier. Exit if fine.
-npm run prettier-check && exit
+npm run prettier:check && exit
 
 # Write by prettier
-npm run prettier-write
+npm run prettier:write
 
 # Print diff
 git --no-pager diff --color | perl /usr/share/doc/git/contrib/diff-highlight/diff-highlight
