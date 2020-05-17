c5f452ea12a2a3bc7d77f87bd73104b7804788ad
AirQuick <AirQuick@users.noreply.github.com>
2020-05-17 11:06:14 +0900

    ci: workaround for Azure Pipelines Java 11 (#962)

--- test/ci/switch-java.cmd
+++ test/ci/switch-java.cmd
@@ -3,4 +3,10 @@ if not defined AGENT_OSARCHITECTURE exit /b 1
 rem "call" is to resolve the target environment variable such as JAVA_HOME_11_X64 dynamically
 call set "JAVA_HOME=%%JAVA_HOME_%~1_%AGENT_OSARCHITECTURE%%%"
 
+rem TODO: Remove this workaround. https://github.com/actions/virtual-environments/issues/885
+if %~1 EQU 11 set "JAVA_HOME=%ProgramFiles%\Java\zulu-11-azure-jdk_11.33.15-11.0.4-win_%AGENT_OSARCHITECTURE%"
+
 path %JAVA_HOME%\bin;%PATH%
+
+rem Verify
+javac -version 2>&1 | "%SYSTEMROOT%\system32\find" " %~1."
