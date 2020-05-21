6339b1ac231d362d391e85426937af3dd498a380
AirQuick <AirQuick@users.noreply.github.com>
2020-05-22 04:50:30 +0900

    ci: specify codespell in requirements-dev.txt

--- test/ci/run-codespell.cmd
+++ test/ci/run-codespell.cmd
@@ -3,7 +3,7 @@ echo Run codespell
 pip install ^
     --disable-pip-version-check ^
     --quiet ^
-    codespell
+    --requirement requirements-dev.txt
 
 rem ".git" dir is not skipped by default: codespell-project/codespell#783
 rem Skipping nested dirs needs "./": codespell-project/codespell#99
--- test/ci/run-codespell.sh
+++ test/ci/run-codespell.sh
@@ -6,7 +6,7 @@ pip install \
     --disable-pip-version-check \
     --quiet \
     --user \
-    codespell
+    --requirement requirements-dev.txt
 
 # ".git" dir is not skipped by default: codespell-project/codespell#783
 # Skipping nested dirs needs "./": codespell-project/codespell#99
