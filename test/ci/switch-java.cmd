c8d37d1b7c3f46d53546f03ca74b7fc802107e35
AirQuick <AirQuick@users.noreply.github.com>
2020-06-27 00:05:18 +0900

    ci: use JavaToolInstaller task on Azure Pipelines (#1016)

--- test/ci/switch-java.cmd
+++
@@ -1,9 +0,0 @@
-if not defined AGENT_OSARCHITECTURE exit /b 1
-
-rem "call" is to resolve the target environment variable such as JAVA_HOME_11_X64 dynamically
-call set "JAVA_HOME=%%JAVA_HOME_%~1_%AGENT_OSARCHITECTURE%%%"
-
-path %JAVA_HOME%\bin;%PATH%
-
-rem Verify
-javac -version 2>&1 | "%SYSTEMROOT%\system32\find" " %~1."
