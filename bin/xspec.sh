4b4ae54b62aa9c6b105b6cac37fd2b05bf95d21a
Amanda Galtman <40716346+galtm@users.noreply.github.com>
2025-04-08 08:19:40 -0400

    feat: make XSpec report its own version number (#2097)
    * Changes from 1146
    
    After bringing files up to date with latest XSpec code
    
    * Include version-utils in reporter stylesheets
    
    * Version info in coverage report
    
    - Add Saxon version, which affects coverage reporting
    - Use CSS float:right for version info in test result report and coverage report
    
    From @birdya22 in https://github.com/xspec/xspec/issues/2067#issuecomment-2668194225
    
    Co-Authored-By: birdya22 <168069809+birdya22@users.noreply.github.com>
    
    * Normalize version in end-to-end reports
    
    * Regenerate end-to-end coverage reports
    
    Include div class="byline" with placeholder data
    
    * Regenerate end-to-end test result reports
    
    Include div class="byline" with placeholder data
    
    * Workaround for Saxon 10 with XProc harness
    
    * Move version number to initial check
    
    Initial check message provides consistency
    across XSLT, Schematron, and XQuery.
    
    * Update bats tests + expected results
    
    - XSpec version for XSLT, XQuery, and Schematron
    - Accommodate extra output line
    - XSLT-specific message does not include XSpec version
    
    * Regenerate expected results after merge
    
    Include version number
    
    ---------
    
    Co-authored-by: birdya22 <168069809+birdya22@users.noreply.github.com>

--- bin/xspec.sh
+++ bin/xspec.sh
@@ -25,6 +25,8 @@ usage() {
         echo "$1"
         echo
     fi
+    echo "XSpec v${XSPEC_VERSION}"
+    echo
     echo "Usage: xspec [-t|-q|-s|-c|-j|-catalog file|-e|-h] file"
     echo
     echo "  file           the XSpec document"
@@ -85,7 +87,9 @@ if test \! -d "${XSPEC_HOME}"; then
     echo "ERROR: XSPEC_HOME is not a directory: ${XSPEC_HOME}"
     exit 1
 fi
-if test \! -f "${XSPEC_HOME}/src/compiler/base/main.xsl"; then
+unset XSPEC_VERSION
+XSPEC_VERSION=$(cat "${XSPEC_HOME}/src/common/VERSION")
+if [ -z "${XSPEC_VERSION}" ]; then
     echo "ERROR: XSPEC_HOME seems to be corrupted: ${XSPEC_HOME}"
     exit 1
 fi
